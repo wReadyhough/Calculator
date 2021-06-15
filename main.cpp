e902e052382b27511063d2bfdd757efd36b02460
willReadyhough <66326631+wReadyhough@users.noreply.github.com>
2021-06-15 12:55:24 +0000

    Added new main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,32 @@
-#include <iostream>
 
-int main() {
-  std::cout << "Hello World!\n";
+#include "calculator.h"
+
+
+//Available operators so far: +,-,*,/,(,),%,^
+
+
+int main(){
+    string equation;
+    vector<string> parsed;
+    string result;
+    while(true){
+        cout<<"Enter your equation or QUIT: ";
+        getline(cin,equation);
+        if(equation == "QUIT"){
+            break;
+        }
+        try{
+            parsed = parser(equation, result);
+            result = calc(parsed);
+            cout<<"The result of your equation is: "<<result<<endl;
+        }
+        catch(int errorCode){
+            equation ="";
+            parsed.clear();
+        }
+    }
+    return 0;
 }
+
+
+
